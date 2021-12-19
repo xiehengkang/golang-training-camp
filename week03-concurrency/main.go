bb47fabbc18ad50d8892126ff901a88737af9774
xiehengkang <xiehengkang@qq.com>
2021-12-19 23:52:49 +0800

    week03homework-pus

---
+++ week03-concurrency/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"golang.org/x/sync/errgroup"
+)
+
+
+
+
+func main() {
+	g, ctx := errgroup.WithContext(context.Background())
+
+	g.Go(func() error {
+		return httpHandle(ctx, ":8000")
+	})
+	g.Go(func() error {
+		return httpHandle(ctx, ":8080")
+	})
+	if err := g.Wait(); err != nil {
+		fmt.Printf("wait over: %v\n", err)
+	}
+}
+
+
+func httpHandle(ctx context.Context, addr string) error {
+	var err error
+	mux := http.NewServeMux()
+	done := make(chan int)
+	mux.HandleFunc("/test", func(w http.ResponseWriter, req *http.Request) {
+		done <- 1
+	})
+	server := http.Server{Addr: addr, Handler: mux}
+	go func() {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("ctx over,err is :%v", ctx.Err())
+		case <-done:
+			fmt.Printf("server[%s] done", addr)
+		}
+	}()
+	err = server.Shutdown(context.Background())
+	return err
+
+}
